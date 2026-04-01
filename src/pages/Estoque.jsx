169eba41dc5460c4ff30c77c1ccc45f1f8600b9c
Daniel L. T. Gugel <daniel.lgugel@outlook.com>
2026-04-01 14:38:00 -0300

    refactor(ui): fix SVG attributes and apply theme-based coloring

--- src/pages/Estoque.jsx
+++ src/pages/Estoque.jsx
@@ -3,8 +3,6 @@ import { useNavigate } from "react-router-dom";
 import { useApp } from "../context/AppContext";
 import { temas } from "../theme";
 import ModalPin from "../components/ModalPin";
-import Deletar from "../assets/apagar.png";
-import Editar from "../assets/editar.png";
 
 // Importing the API service functions to interact with the backend
 import { getProdutos } from "../services/produtos.js";
@@ -17,6 +15,7 @@ export default function Estoque() {
     const isSonhoInfantil = loja === "sonhoInfantil";
     const tipo = isSonhoInfantil ? "roupas" : "perfumes";
     const t = temas[loja] || temas.sonhoInfantil;
+    const corIcons = isSonhoInfantil ? "#171717" : "#f5f5f5";
 
     const [busca, setBusca] = useState("");
     const [filtroGenero, setFiltroGenero] = useState("Todos");
@@ -182,22 +181,56 @@ export default function Estoque() {
                                     </div>
                                     <div className="flex flex-row">
                                         <div
-                                            className="mt-3 w-5 h-5 cursor-pointer ml-auto"
+                                            className="mt-3 w-7 h-7 cursor-pointer ml-auto"
                                             onClick={() => {
                                                 setAcaoPin("editar");
                                                 setModalPin(true);
                                             }}
                                         >
-                                            <img src={Editar} alt="Editar" />
+                                            <svg
+                                                viewBox="0 0 24 24"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                className="w-full h-full"
+                                            >
+                                                <path
+                                                    d="M20,16v4a2,2,0,0,1-2,2H4a2,2,0,0,1-2-2V6A2,2,0,0,1,4,4H8"
+                                                    fill="none"
+                                                    stroke={`${corIcons}`}
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    strokeWidth="2"
+                                                />
+
+                                                <polygon
+                                                    fill="none"
+                                                    points="12.5 15.8 22 6.2 17.8 2 8.3 11.5 8 16 12.5 15.8"
+                                                    stroke={`${corIcons}`}
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    strokeWidth="2"
+                                                />
+                                            </svg>
                                         </div>
                                         <div
-                                            className="mt-3 w-5 h-5 cursor-pointer ml-auto"
+                                            className="mt-3 w-7 h-7 cursor-pointer ml-auto"
                                             onClick={() => {
                                                 setAcaoPin("deletar");
                                                 setModalPin(true);
                                             }}
                                         >
-                                            <img src={Deletar} alt="Deletar" />
+                                            <svg
+                                                viewBox="0 0 24 24"
+                                                fill="none"
+                                                stroke={`${corIcons}`}
+                                                className="w-full h-full"
+                                            >
+                                                <path
+                                                    d="M10 12L14 16M14 12L10 16M18 6L17.1991 18.0129C17.129 19.065 17.0939 19.5911 16.8667 19.99C16.6666 20.3412 16.3648 20.6235 16.0011 20.7998C15.588 21 15.0607 21 14.0062 21H9.99377C8.93927 21 8.41202 21 7.99889 20.7998C7.63517 20.6235 7.33339 20.3412 7.13332 19.99C6.90607 19.5911 6.871 19.065 6.80086 18.0129L6 6M4 6H20M16 6L15.7294 5.18807C15.4671 4.40125 15.3359 4.00784 15.0927 3.71698C14.8779 3.46013 14.6021 3.26132 14.2905 3.13878C13.9376 3 13.523 3 12.6936 3H11.3064C10.477 3 10.0624 3 9.70951 3.13878C9.39792 3.26132 9.12208 3.46013 8.90729 3.71698C8.66405 4.00784 8.53292 4.40125 8.27064 5.18807L8 6"
+                                                    strokeWidth="2"
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                />
+                                            </svg>
                                         </div>
                                     </div>
                                 </div>
